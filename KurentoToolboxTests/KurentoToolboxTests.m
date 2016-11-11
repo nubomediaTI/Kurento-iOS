187d9892c23f3f3d9074b1a41ce35343b1eae713
Massimo Milazzo <massimo@Massimos-MacBook-Pro.local>
2016-11-11 12:05:06 +0100

    Added the Apache 2.0 license info header to all project files and removed other Copyright info

--- KurentoToolboxTests/KurentoToolboxTests.m
+++ KurentoToolboxTests/KurentoToolboxTests.m
@@ -1,10 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
 //
-//  KurentoToolboxTests.m
-//  KurentoToolboxTests
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Created by Marco Rossi on 06/11/15.
-//  Copyright © 2015 Telecom Italia S.p.A. All rights reserved.
-//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 #import <XCTest/XCTest.h>
 #import "KurentoToolbox.h"
